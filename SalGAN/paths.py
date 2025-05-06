3c2a06ffc6d21e883d82b6b3acd8cbbc764f6554
zedyz <fredrikolsen94@gmail.com>
2025-05-06 08:38:17 +0200

    added websaliency trained salgan implementation

---
+++ SalGAN/paths.py
@@ -0,0 +1,14 @@
+#Original Data Path
+imagesTrain = "data/images/train/"
+imagesVal = "data/images/val/"
+imagesTest = "data/images/val/"
+mapsTrain = "data/maps/train/"
+mapsVal = "data/maps/val/"
+
+#Resized Data Path
+resizedImagesTrain = "resized/images/train/"
+resizedImagesVal = "resized/images/val/"
+resizedImagesTest = "resized/images/val/"
+resizedMapsTrain = "resized/maps/train/"
+resizedMapsVal = "resized/maps/val/"
+
